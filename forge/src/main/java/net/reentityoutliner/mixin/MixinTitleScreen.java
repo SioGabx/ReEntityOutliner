5573311f628cccdd6c6fb961334bc4ef11a4cb9d
SioGabx <41244237+SioGabx@users.noreply.github.com>
2025-07-23 21:08:42 +0200

    added Forge mod version

---
+++ forge/src/main/java/net/reentityoutliner/mixin/MixinTitleScreen.java
@@ -0,0 +1,17 @@
+package net.reentityoutliner.mixin;
+
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.reentityoutliner.Constants;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class MixinTitleScreen {
+
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        Constants.LOG.info("Forge : Loaded Re:Entity Outliner");
+    }
+}
