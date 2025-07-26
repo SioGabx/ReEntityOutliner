d20b242b0e67a67195c03cd4ee3bcfe97c0c3043
SioGabx <41244237+SioGabx@users.noreply.github.com>
2025-07-26 20:12:44 +0200

    Missing code that override the entity color if they are in a team

--- common/src/main/java/net/reentityoutliner/mixin/MixinEntity.java
+++ common/src/main/java/net/reentityoutliner/mixin/MixinEntity.java
@@ -2,6 +2,7 @@ package net.reentityoutliner.mixin;
 
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,20 +13,40 @@ import static net.reentityoutliner.config.ConfigManager.*;
 
 @Mixin(Entity.class)
 public abstract class MixinEntity {
-    @Shadow public abstract EntityType<?> getType();
+    @Shadow
+    public abstract EntityType<?> getType();
 
     @Inject(at = @At("HEAD"), method = "getTeamColor()I", cancellable = true)
     private void onGetTeamColor(CallbackInfoReturnable<Integer> info) {
-        if (isOutliningEntities()){
-        var settings = outlinedEntityTypes.get(this.getType());
-        if (settings.outlined) {
+        if (isOutliningEntities()) {
+            var settings = outlinedEntityTypes.get(this.getType());
+            if (settings.outlined) {
+                //Get generic colors for entity
                 int red = settings.color.red;
                 int green = settings.color.green;
                 int blue = settings.color.blue;
 
+                //Make outline to the color of the player
+                if (this.getType() == EntityType.PLAYER) {
+                    if (((Object)this) instanceof Player player) {
+                        var team = player.getTeam();
+                        if (team != null) {
+                            var colorValue = team.getColor().getColor();
+                            if (colorValue != null) {
+                                int argbInt = colorValue;
+                                red = (argbInt >> 16) & 0xFF;
+                                green = (argbInt >> 8) & 0xFF;
+                                blue = argbInt & 0xFF;
+                            }
+                        }
+                    }
+                }
+
+                //Apply
                 int color = (255 << 24) | (red << 16) | (green << 8) | blue;
                 info.setReturnValue(color);
                 info.cancel();
-        }}
+            }
+        }
     }
 }
