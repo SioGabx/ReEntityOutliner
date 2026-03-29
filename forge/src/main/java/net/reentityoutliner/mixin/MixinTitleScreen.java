d84b8b7b704127e4f7243c1bac87c8a3211b8e9c
SioGabx <41244237+SioGabx@users.noreply.github.com>
2026-03-29 15:30:06 +0200

    Started updating to 26.1

--- forge/src/main/java/net/reentityoutliner/mixin/MixinTitleScreen.java
+++ forge/src/main/java/net/reentityoutliner/mixin/MixinTitleScreen.java
@@ -1,7 +1,7 @@
 package net.reentityoutliner.mixin;
 
-import net.minecraft.client.gui.screens.TitleScreen;
 import net.reentityoutliner.Constants;
+import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
--- forge/src/main/java/net/reentityoutliner/platform/ForgePlatformHelper.java
+++ forge/src/main/java/net/reentityoutliner/platform/ForgePlatformHelper.java
@@ -1,8 +1,8 @@
 package net.reentityoutliner.platform;
 
 import net.reentityoutliner.platform.services.IPlatformHelper;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.loading.LoadingModList;
 
 public class ForgePlatformHelper implements IPlatformHelper {
 
@@ -15,7 +15,7 @@ public class ForgePlatformHelper implements IPlatformHelper {
     @Override
     public boolean isModLoaded(String modId) {
 
-        return ModList.get().isLoaded(modId);
+        return LoadingModList.getModFileById(modId) != null;
     }
 
     @Override
