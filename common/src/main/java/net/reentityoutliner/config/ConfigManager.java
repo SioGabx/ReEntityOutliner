4422592b85afcb8ab6d342bedb0ebfd206ec5404
SioGabx <41244237+SioGabx@users.noreply.github.com>
2026-06-01 13:59:41 +0200

    ReEntityOutliner now restore IsOutliningEntities + Updated version number

--- common/src/main/java/net/reentityoutliner/config/ConfigManager.java
+++ common/src/main/java/net/reentityoutliner/config/ConfigManager.java
@@ -40,6 +40,7 @@ public class ConfigManager {
         if (client.player != null) {
         String message = outliningEntities ? "gui.re-entity-outliner.outline.now-on" : "gui.re-entity-outliner.outline.now-off";
         client.player.displayClientMessage(Component.translatable(message, Constants.MOD_NAME), true);
+        save();
         }
     }
 
@@ -82,6 +83,7 @@ public class ConfigManager {
             }
         }
 
+        config.addProperty("isOutliningEntities", isOutliningEntities());
         config.add("outlinedEntities", outlinedEntitiesArray);
 
         try {
@@ -135,6 +137,9 @@ public class ConfigManager {
 
             String jsonString = Files.readString(getConfigPath());
             JsonObject config = GSON.fromJson(jsonString, JsonObject.class);
+            if (config != null && config.has("isOutliningEntities")) {
+                outliningEntities = config.getAsJsonPrimitive("isOutliningEntities").getAsBoolean();
+            }
 
             if (config != null && config.has("outlinedEntities")) {
                 JsonArray outlinedEntitiesArray = config.getAsJsonArray("outlinedEntities");
