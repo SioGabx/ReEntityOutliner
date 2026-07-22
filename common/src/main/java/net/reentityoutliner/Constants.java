687119d20f67833e03c7275f6eebfc352a4a20b5
personalized-advertising <265311956+personalized-advertising@users.noreply.github.com>
2026-07-22 17:04:03 +0100

    Update Constants.java

--- common/src/main/java/net/reentityoutliner/Constants.java
+++ common/src/main/java/net/reentityoutliner/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
 
 	public static final String MOD_ID = "reentityoutliner";
 	public static final String MOD_NAME = "Re:EntityOutliner";
+	public static final String SERVER_DISABLE_CHANNEL = "reentityoutliner:disable";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
 }
